cb5834a5f9da046a75a5271e749bf76faeaa1a7d
Amir Kolahi <167548202+echoamir@users.noreply.github.com>
2025-11-01 08:25:05 +0330

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -11,8 +11,8 @@ sudo systemctl enable --now docker
 sudo usermod -aG docker ${USER}
 
 latest_compose_version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K[^\"]+')
-sudo curl -L "https://github.com/docker/compose/releases/download/${latest_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${latest_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/compose
+sudo chmod +x /usr/local/bin/compose
 echo ""
 docker --version
 docker-compose --version
